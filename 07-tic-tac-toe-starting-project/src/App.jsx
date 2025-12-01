2c417c504bcb75d5a5a08ad64c68308fe1bd8a17
niyankjain <jainniyank@gmail.com>
2025-12-02 00:46:05 +0530

    Enhance Gameboard and Player component

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,18 +1,26 @@
-import Player from "./component/Player";
+import { useState } from "react";
+import GameBoard from "./component/GameBoard.jsx";
+import Player from "./component/Player.jsx";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+  // const [log, setLog] = useState('')
+
+  function handleSelectButton() {
+    setActivePlayer(currentActivePlayer => currentActivePlayer === 'X' ? '0': 'X');
+  }
   
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X"/>          
-          <Player initialName="Player 2" symbol="0"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive = {activePlayer === 'X'}/>          
+          <Player initialName="Player 2" symbol="0" isActive = {activePlayer === '0'}/>
         </ol>
-        Game Board
+        <GameBoard onSelectedButton={handleSelectButton} activePlayerSymbol={activePlayer}/>
       </div>
-
-      Log
+      LOG
+      
     </main>
   );
 }
