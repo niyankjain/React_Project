879c19f9fe1befbb67d73b2bafeec3590cd10ade
niyankjain <jainniyank@gmail.com>
2025-12-08 00:36:52 +0530

    Create Log in game

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,28 +1,70 @@
 import { useState } from "react";
 import GameBoard from "./component/GameBoard.jsx";
 import Player from "./component/Player.jsx";
+import Log from "./component/Log.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
-function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
-  // const [log, setLog] = useState('')
+function deriveActivePlayer(gameTurn) {
+  let currentPlayer ='X';
+  if(gameTurn && gameTurn.length>0 && gameTurn[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
 
-  function handleSelectButton() {
-    setActivePlayer(currentActivePlayer => currentActivePlayer === 'X' ? '0': 'X');
+const initialGameBoard = [
+[null, null, null],
+[null, null, null],
+[null, null, null]
+];
+
+export default function App() {
+  const [gameTurn, setGameTurn] = useState([]);
+  const currentActivePlayer =deriveActivePlayer(gameTurn);
+
+
+  let gameBoard = initialGameBoard;
+    for(const turn of gameTurn) {
+        const{square, player} = turn;
+        const {rowIndex, colIndex} = square;
+        gameBoard[rowIndex][colIndex] = player;
+    }
+
+  
+  function handleSelectButton(rowIndex, colIndex) {
+
+    // for(const combinations of WINNING_COMBINATIONS) {
+    //   const firstRowPlayer = gameBoard[combinations[0].row]
+    // }
+
+
+    setGameTurn(prevTurn => {
+      
+      const currentPlayer = deriveActivePlayer(prevTurn);
+      let newTurn = {
+            "square": {
+              "rowIndex": rowIndex, 
+              "colIndex": colIndex 
+            },
+            "player": currentPlayer
+      };
+      const updatedTurns = [newTurn, ...prevTurn];
+      return updatedTurns;
+    })
   }
   
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive = {activePlayer === 'X'}/>          
-          <Player initialName="Player 2" symbol="0" isActive = {activePlayer === '0'}/>
+          <Player initialName="Player 1" symbol="X" isActive = {currentActivePlayer === 'X'}/>          
+          <Player initialName="Player 2" symbol="0" isActive = {currentActivePlayer === '0'}/>
         </ol>
-        <GameBoard onSelectedButton={handleSelectButton} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectedButton={handleSelectButton} boards={gameBoard}/>
       </div>
-      LOG
+      
+        <Log turns={gameTurn }/>
       
     </main>
   );
 }
-
-export default App
--- 07-tic-tac-toe-starting-project/src/component/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/component/GameBoard.jsx
@@ -1,32 +1,13 @@
-import { useState } from "react";
-
-const initialGameBoard = [
-[null, null, null],
-[null, null, null],
-[null, null, null]
-];
-
-export default function GameBoard({onSelectedButton, activePlayerSymbol}) {
-    const[gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSeletedButton(rowIndex, columnIndex) {
-        setGameBoard ((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map((innerArray => [...innerArray]))];
-            updatedBoard[rowIndex][columnIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-
-        onSelectedButton();
-    }
-
+export default function GameBoard({onSelectedButton, boards}) {
+   
     return (
         <ol id="game-board">
-            {gameBoard.map ((row, rowIndex) =>
+            {boards.map ((row, rowIndex) =>
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, columnIndex) =>
                             <li key={columnIndex}> 
-                                <button onClick={()=>handleSeletedButton(rowIndex, columnIndex)}>{playerSymbol}</button>
+                               <button onClick={()=>onSelectedButton(rowIndex, columnIndex)} disabled= {playerSymbol !== null}>{playerSymbol}</button> 
                             </li>
                         ) }
                     </ol>
